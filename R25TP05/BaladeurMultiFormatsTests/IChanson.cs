c1541df2c952d5d4eb6a4e0727c6082aaef56669
Zachary Tanguay <2176651@cegepmontpetit.ca>
2022-05-04 09:55:08 -0400

    FCT : TODO #02 Ajout de l’interface IChanson
    Définir l’interface IChanson

---
+++ R25TP05/BaladeurMultiFormatsTests/IChanson.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaladeurMultiFormatsTests
+{
+    interface IChanson
+    {
+        #region Propriétés
+        /// <summary>
+        /// Obtient l’année de création de la chanson 
+        /// </summary>
+        int Annee { get; }
+
+        /// <summary>
+        /// Obtient le nom de l’artiste ou du groupe ayant créé la chanson
+        /// </summary>
+        string Artiste { get; }
+
+        /// <summary>
+        /// Obtient le format audio de la chanson par exemple AAC, MP3 ou WMA
+        /// </summary>
+        string Format { get; }
+
+        /// <summary>
+        /// Obtient le nom de fichier de la chanson
+        /// </summary>
+        string NomFichier { get; }
+
+        /// <summary>
+        /// Cette propriété calculée va obtenir les paroles de la chanson à partir d’un fichier texte
+        /// </summary>
+        string Paroles { get; }
+
+        /// <summary>
+        /// Obtient le titre de la chanson
+        /// </summary>
+        string Titre { get; }
+        #endregion
+
+        #region Méthodes
+        void Ecrire(string pParoles);
+
+        void EcrireEntete(StreamWriter pobjFichier, string pParoles);
+
+        void EcrireParoles(StreamWriter pobjFichier, string pParoles);
+
+        void LireEntete();
+
+        string LireParoles(StreamReader pobjFichier);
+
+        void SauterEntete(StreamReader pobjFichier);
+        #endregion
+    }
+}
