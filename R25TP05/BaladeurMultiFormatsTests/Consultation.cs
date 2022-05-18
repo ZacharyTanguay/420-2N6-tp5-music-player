ccda560fb2486f7e457c215bb0f89c7aec9b0302
Zack <2176651@cegepmontpetit.ca>
2022-05-17 22:42:02 -0400

    FCT : TODO #14 Ajout des tests pour la classe Consultation
    Définir les 4 tests pour la classe Consultation

---
+++ R25TP05/BaladeurMultiFormatsTests/Consultation.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace BaladeurMultiFormats
+{
+    /// <summary>
+    /// Modélise une consultation de chanson.
+    /// </summary>
+    public class Consultation
+    {
+        #region Propriétés
+        /// <summary>
+        /// Date de la consultation
+        /// </summary>
+        public DateTime Date { get; }
+        /// <summary>
+        /// Chanson consultée
+        /// </summary>
+        public object LaChanson { get; }
+        /// <summary>
+        /// Calcul le délai en la date actuelle et la date de consultation de la chanson
+        /// </summary>
+        public int Délai
+        {
+            get
+            {
+                return (int)((DateTime.Now - Date).TotalSeconds);
+            }
+        }
+        #endregion
+
+        #region Constructeur
+        /// <summary>
+        /// Initialise une consultation de chanson
+        /// </summary>
+        /// <param name="pDate">Date au moment de la consulation</param>
+        /// <param name="pChanson">Chanson qui a été consultée</param>
+        public Consultation(DateTime pDate, object pChanson)
+        {
+            if (pChanson == null) throw new ArgumentNullException();
+            Date = pDate;
+            LaChanson = pChanson;
+        }
+        #endregion
+    }
+}
