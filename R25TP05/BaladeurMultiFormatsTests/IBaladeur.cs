4236d2ecdd10eb92c89b86e97d0e0aac066e0ba1
Zack <2176651@cegepmontpetit.ca>
2022-05-17 21:24:34 -0400

    FCT : TODO #07 Ajout de l’interface IBaladeur
    Définir la classe Baladeur sans compléter le code des méthodes

---
+++ R25TP05/BaladeurMultiFormatsTests/IBaladeur.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using BaladeurMultiFormats;
+
+namespace BaladeurMultiFormatsTests
+{
+    public interface IBaladeur
+    {
+        int NbChansons { get; }
+
+        void AfficherLesChansons(ListView pListView);
+
+        Chanson ChansonAt(int pIndex);
+
+        void ConstruireLaListeDesChansons();
+
+        void ConvertirVersAAC(int pIndex);
+
+        void ConvertirVersMP3(int pIndex);
+
+        void ConvertirVersWMA(int pIndex);
+    }
+}
